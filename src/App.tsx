a2c018bf2eeeb079ccea6c7e4aaee1d383c2ad5b
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-09-04 21:04:04 -0300

    config inicial

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import ListaVagas from './containers/ListaVagas'
 import './global.css'
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-      <div className="container">
-        <ListaVagas />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Hero />
+            <div className="container">
+                <ListaVagas />
+            </div>
+        </>
+    )
 }
 
 export default App
